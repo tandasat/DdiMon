fc1809fced4a9b06e95038a6e1be9c84a43576ac
tandasat <tanda.sat@gmail.com>
2016-04-13 17:45:59 -0700

    update clean.bat:

--- clean.bat
+++ clean.bat
@@ -1,12 +1,15 @@
 @echo off
 del *.sdf
+del *.VC.db
 del /s *.aps
 del /a:h *.suo
 rmdir /s /q .vs
 rmdir /s /q ipch
 rmdir /s /q x64
 rmdir /s /q Debug
+rmdir /s /q Debug_WDK
 rmdir /s /q Release
+rmdir /s /q Release_WDK
 rmdir /s /q DdiMon\x64
 rmdir /s /q DdiMon\Debug
 rmdir /s /q DdiMon\Release
